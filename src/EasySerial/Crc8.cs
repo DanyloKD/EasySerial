558c8310b34ae7a001fbbeca29428f3bfd092087
Danylo <danylo.kd@gmail.com>
2020-08-30 21:54:37 +0200

    Add CRC 8bit calculation

---
+++ src/EasySerial/Crc8.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace EasySerial
+{
+    public class Crc8 
+    {
+        public static int ChecksumLength = sizeof(byte);
+
+        private readonly byte polinominal;
+        private readonly byte[] table;
+        
+        public Crc8(byte polinominal = 0x1D)
+        {
+            this.polinominal = polinominal;
+            this.table = GenerateTable(this.polinominal);
+        }
+
+        public byte Calculate(ReadOnlySpan<byte> input)
+        {
+            byte crc = 0;
+            foreach (var b in input)
+            {
+                byte index = (byte)(b ^ crc);
+                crc = table[index];
+            }
+
+            return crc;
+        }
+
+        public bool Verify(ReadOnlySpan<byte> input)
+        {
+            return Calculate(input) == 0x00;
+        }
+
+        private byte[] GenerateTable(byte polinominal)
+        {
+            const byte MsbMask = 1 << (sizeof(byte) * 8 - 1);
+            
+            var table = new byte[0xFF + 1];
+            for (var inputByte = 0x01; inputByte != 0xFF; inputByte++)
+            {
+                // T crc = (T)(inputByte << ((sizeof(T) - 1)  * 8));
+                byte crc = (byte)inputByte;
+                for (int j = 8; j > 0; j--)
+                {
+                    if ((crc & MsbMask) != 0)
+                    {
+                        crc <<= 1;
+                        crc ^= polinominal;
+                    }
+                    else 
+                    {
+                        crc <<= 1;
+                    }
+                }
+
+                table[inputByte] = crc;
+            }
+
+            return table;
+        }
+
+    }
+}
---
+++ tests/EasySerial.Tests/Crc8Tests.cs
@@ -0,0 +1,59 @@
+﻿using Xunit;
+
+namespace EasySerial.Tests
+{
+    public class Crc8Tests
+    {
+        [Fact]
+        public void Calculate_Works()
+        {
+            var calcualtor = new Crc8();
+            var data = new byte[] { 0xC2 };
+            var crc = calcualtor.Calculate(data);
+
+            Assert.Equal(0x0F, crc);
+        }
+
+        [Fact]
+        public void Calculate_WithEmptyInput_ReturnsPolinominal()
+        {
+            var calcualtor = new Crc8();
+            var crc1 = calcualtor.Calculate(new byte[0]);
+            var crc2 = calcualtor.Calculate(null);
+
+            Assert.Equal(0, crc1);
+            Assert.Equal(0, crc2);
+        }
+
+        [Fact]
+        public void Verify_WithValidInput_Pass()
+        {
+            var calcualtor = new Crc8();
+            var data = new byte[] { 0xC2, 0x0F };
+            var result = calcualtor.Verify(data);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Verify_WithChecksumOnly_Pass()
+        {
+            const int Polinominal = 0x42;
+            var calcualtor = new Crc8(Polinominal);
+            var data = new byte[] { 0x00 };
+            var result = calcualtor.Verify(data);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Verify_WithInvalidChecksum_Fails()
+        {
+            var calcualtor = new Crc8();
+            var data = new byte[] { 0xC2, 0xBE /* invalid, correct is 0x0F */ };
+            var result = calcualtor.Verify(data);
+
+            Assert.False(result);
+        }
+    }
+}
