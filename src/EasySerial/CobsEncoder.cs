30e9cdaf68faa64c6047d442cecaa6441400c3f0
Danylo <danylo.kd@gmail.com>
2020-08-30 22:00:18 +0200

    Fix typo in variable name

--- src/EasySerial/CobsEncoder.cs
+++ src/EasySerial/CobsEncoder.cs
@@ -19,39 +19,39 @@ namespace EasySerial
             var writePos = 1;
             var chunkPos = 0;
 
-            byte chunkLenth = 1;
+            byte chunkLength = 1;
 
             while (readPos < inputLength)
             {
                 if (raw[readPos] != DELIMITER)
                 {
-                    if (chunkLenth == MAX_CHUNK_LENGTH)
+                    if (chunkLength == MAX_CHUNK_LENGTH)
                     {
                         output[chunkPos] = MAX_CHUNK_LENGTH;
                         chunkPos = writePos;
-                        chunkLenth = 1;
+                        chunkLength = 1;
                         writePos++;
                         // readPos++;
                     }
                     else
                     {
                         output[writePos] = raw[readPos];
-                        chunkLenth++;
+                        chunkLength++;
                         writePos++;
                         readPos++;
                     }
                 }
                 else
                 {
-                    output[chunkPos] = chunkLenth;
+                    output[chunkPos] = chunkLength;
                     chunkPos = writePos;
-                    chunkLenth = 1;
+                    chunkLength = 1;
                     writePos++;
                     readPos++;
                 }
             }
 
-            output[chunkPos] = chunkLenth;
+            output[chunkPos] = chunkLength;
             output[writePos] = DELIMITER;
 
             return writePos + 1;
